9fa256d44311eff0f9d76507f767d19428768745
Erick E. Tavares <erick-tavares@hotmail.com>
2020-11-29 18:35:11 -0300

    Movimentacao do bispo

--- src/main/java/xadrez/pecas/Bispo.java
+++ src/main/java/xadrez/pecas/Bispo.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaDeXadrez;
@@ -16,6 +17,48 @@ public class Bispo extends PecaDeXadrez {
     }
 
     public boolean[][] movimentosPossiveis() {
-        return new boolean[0][];
+        boolean[][] movimentosVerdadeiros = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+        Posicao p = new Posicao(0,0);
+        //noroeste
+        p.setValues(p.getLinha() - 1,p.getColuna() - 1);
+        while (getTabuleiro().posicaoExistente(p) && !getTabuleiro().existePecaNaPosicao(p)){
+            movimentosVerdadeiros[p.getLinha()][p.getColuna()] = true;
+            p.setValues(p.getLinha() - 1, p.getColuna() - 1);
+        }
+        if (getTabuleiro().posicaoExistente(p) && haPecaAdversariaNaPosicao(p)){
+            movimentosVerdadeiros[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //nordeste
+        p.setValues(p.getLinha() - 1,p.getColuna() + 1);
+        while (getTabuleiro().posicaoExistente(p) && !getTabuleiro().existePecaNaPosicao(p)){
+            movimentosVerdadeiros[p.getLinha()][p.getColuna()] = true;
+            p.setValues(p.getLinha() - 1, p.getColuna() + 1);
+        }
+        if (getTabuleiro().posicaoExistente(p) && haPecaAdversariaNaPosicao(p)){
+            movimentosVerdadeiros[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //sudeste
+        p.setValues(p.getLinha() + 1,p.getColuna() + 1);
+        while (getTabuleiro().posicaoExistente(p) && !getTabuleiro().existePecaNaPosicao(p)){
+            movimentosVerdadeiros[p.getLinha()][p.getColuna()] = true;
+            p.setValues(p.getLinha() + 1, p.getColuna() + 1);
+        }
+        if (getTabuleiro().posicaoExistente(p) && haPecaAdversariaNaPosicao(p)){
+            movimentosVerdadeiros[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //sudoeste
+        p.setValues(p.getLinha() + 1,p.getColuna() - 1);
+        while (getTabuleiro().posicaoExistente(p) && !getTabuleiro().existePecaNaPosicao(p)){
+            movimentosVerdadeiros[p.getLinha()][p.getColuna()] = true;
+            p.setValues(p.getLinha() + 1, p.getColuna() - 1);
+        }
+        if (getTabuleiro().posicaoExistente(p) && haPecaAdversariaNaPosicao(p)){
+            movimentosVerdadeiros[p.getLinha()][p.getColuna()] = true;
+        }
+        return movimentosVerdadeiros;
     }
 }
